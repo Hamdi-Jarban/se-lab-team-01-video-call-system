2a0db1fb208c084f11e3bbffff5e6082c297f386
Hamdiabd <hamdijarban@gmail.com>
2026-09-25 01:21:14 +0300

    build: update gitignore to exclude bin and obj artifacts

--- VideoCall.Shared/obj/Debug/net8.0/VideoCall.Shared.AssemblyInfo.cs
+++ VideoCall.Shared/obj/Debug/net8.0/VideoCall.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoCall.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f7d29b467fc622ffc5a341bdf143a7a0dc84c23")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoCall.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoCall.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
